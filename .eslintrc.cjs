5d3a224c11947c3d892d52f953d69f8ec0fe914d
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-11-13 12:32:21 -0500

    Add `package.json` and `lib/index.js`.

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'digitalbazaar',
+    'digitalbazaar/jsdoc',
+    'digitalbazaar/module'
+  ],
+  rules: {
+    'unicorn/prefer-node-protocol': 'error'
+  },
+  ignorePatterns: ['node_modules/']
+};
